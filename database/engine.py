46f137bd0cbf3e4d630a239496b295d8e39eedb5
Asylbek86 <alibekjan@mail.com>
2025-01-10 13:36:35 +0300

    2commit

--- database/engine.py
+++ database/engine.py
@@ -2,6 +2,9 @@ import os
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
 from database.models import Base
+from database.orm_query import orm_add_banner_description, orm_create_categories
+
+from common.texts_for_db import categories, description_for_info_pages
 
 #from .env file:
 # DB_LITE=sqlite+aiosqlite:///my_base.db
@@ -9,14 +12,19 @@ from database.models import Base
 
 # engine = create_async_engine(os.getenv('DB_LITE'), echo=True)
 
-engine = create_async_engine(os.getenv('DB_LITE'), echo=True)
+engine = create_async_engine(os.getenv('DB_URL'), echo=True)
 
 session_maker = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+
 
 
 async def create_db():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
+        
+    async with session_maker() as session:
+        await orm_create_categories(session, categories)
+        await orm_add_banner_description(session, description_for_info_pages)
 
 
 async def drop_db():
